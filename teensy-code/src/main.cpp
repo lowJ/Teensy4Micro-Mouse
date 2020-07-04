26869002c806fb4f7fef7a3d7e0068f98c0463db
William Redenbaugh <wredenba@uci.edu>
2020-07-03 21:36:07 -0700

    setup teensy/chibiOS development enviornment

---
+++ teensy-code/src/main.cpp
@@ -0,0 +1,37 @@
+// Arduino and ChibiOS Libraries 
+#include "Arduino.h"
+#include "ChRt.h"
+
+// Our own modules 
+#include "lwip_thread.hpp"
+
+// Other libraries
+
+// Our runtime files. 
+
+/**************************************************************************/
+/*!
+    @brief Where we call our chibiOS initialization functions.
+*/
+/**************************************************************************/
+void chibiSetup(void){
+  // This is technically a runtime thread but it holds a bunch of subthreads to do work 
+  setup_lwip_thread();
+
+}
+
+/**************************************************************************/
+/*!
+    @brief All of our setup happens in here. This is where to start up chibiOS. 
+*/
+/**************************************************************************/
+void setup() {
+  // We do all of our setup in here. 
+  chBegin(chibiSetup);  
+}
+
+
+// Currently doing nothing atm. 
+void loop() {
+  chThdSleepSeconds(1);
+}
