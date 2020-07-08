92e79dd2d7735073f8b2886f9acdc04bbe86b597
Jonathan <jjonathanlow@gmail.com>
2020-07-07 23:56:16 -0700

    start motor code

--- teensy-code/include/motor_controller.hpp
+++ teensy-code/include/motor_controller.hpp
@@ -6,9 +6,32 @@ Last Edit Date: 7/3/2020
 #ifndef _MOTOR_CONTROLLER_HPP
 #define _MOTOR_CONTROLLER_HPP
 
+
+#define M1_SPEED 4
+#define M1_FORWARD 1
+#define M1_BACKWARD 0
+
+#define M2_SPEED 5
+#define M2_FORWARD 2
+#define M2_BACKWARD 3
+
 #include "Arduino.h"
 #include "ChRt.h"
 
 extern void setup_motor_controller(void);
+
+extern void set_m1_dir(uint8_t dir);
+
+extern void set_m2_dir(uint8_t dir);
+
+extern void set_m1_speed(uint8_t dir);
+
+extern void set_m2_speed(uint8_t dir);
+
+extern void m1_stop();
+
+extern void m2_stop();
+
+extern void m1_m2_stop();
 
 #endif
--- teensy-code/src/motor_controller.cpp
+++ teensy-code/src/motor_controller.cpp
@@ -8,5 +8,53 @@ Last Edit Date: 7/3/2020
 extern void setup_motor_controller(void);
 
 extern void setup_motor_controller(void){
+    m1_m2_stop();
+}
 
+
+void set_m1_dir(uint8_t dir){ //0 = backwards, 1 = forward
+    if(dir){ //test if these are correct, if not, reverse
+        digitalWrite(M1_FORWARD, 1);
+        digitalWrite(M1_BACKWARD, 0);
+    }
+    else{
+        digitalWrite(M1_FORWARD, 0);
+        digitalWrite(M1_BACKWARD, 1);
+    }
+}
+
+void set_m2_dir(uint8_t dir){ //0 = backwards, 1 = forward
+    if(dir){ //test if these are correct, if not, reverse
+        digitalWrite(M2_FORWARD, 1);
+        digitalWrite(M2_BACKWARD, 0);
+    }
+    else{
+        digitalWrite(M2_FORWARD, 0);
+        digitalWrite(M2_BACKWARD, 1);
+    }
+}
+
+void set_m1_spd(uint8_t spd){
+    analogWrite(M1_SPEED, spd);
+}
+
+void set_m2_spd(uint8_t spd){
+    analogWrite(M2_SPEED, spd);
+}
+
+void m1_stop(){
+    digitalWrite(M1_FORWARD, 0);
+    digitalWrite(M1_BACKWARD, 0);
+    analogWrite(M1_SPEED, 0);
+}
+
+void m2_stop(){
+    digitalWrite(M2_FORWARD, 0);
+    digitalWrite(M2_BACKWARD, 0);
+    analogWrite(M2_SPEED, 0);
+}
+
+void m1_m2_stop(){
+    m1_stop();
+    m2_stop();
 }
