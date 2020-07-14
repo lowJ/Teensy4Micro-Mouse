ce49a3e4edf0283adde2c6ee8163bacb0345cde2
William Redenbaugh <wredenba@uci.edu>
2020-07-14 16:26:34 -0700

    added motor contorl

--- teensy-code/src/motor_controller.cpp
+++ teensy-code/src/motor_controller.cpp
@@ -8,5 +8,18 @@ Last Edit Date: 7/3/2020
 extern void setup_motor_controller(void);
 
 extern void setup_motor_controller(void){
+    // Setting up the gpio for our motors. 
+    pinMode(MOTOR1_BACKWARD, OUTPUT);
+    pinMode(MOTOR1_FORWARD, OUTPUT);
+    
+    pinMode(MOTOR1_PWM, OUTPUT);
+    analogWriteResolution(8);
+    
+    pinMode(MOTOR2_BACKWARD, OUTPUT);
+    pinMode(MOTOR2_FORWARD, OUTPUT);
+    pinMode(MOTOR2_PWM, OUTPUT);
+}
+
+extern void move_forward(){
 
 }
